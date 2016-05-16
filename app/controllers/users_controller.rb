9f85c27f18a48e045b1a958fc1542f711e2a473b
coltonstaab1 <colton.staab@gmail.com>
2016-05-15 22:00:31 -0400

    Created seed data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def index
-
+    @designers = User.where(designer: 'true').order('name asc')
+    @users = User.where(designer: 'false').order('name asc')
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "Colton", email: "colton@gmail.com", password: "hellocats")
+User.create(name: "Matthew", email: "matthew@gmail.com", password: "hikitty")
+User.create(name: "Kathy", designer: true, email: "kathy@gmail.com", password: "iheartdogs")
+Room.create(user_id: 1, description: "Bedroom", width: 15, depth: 102)
+Room.create(user_id: 1, description: "Bathroom", width: 12, depth: 50)
+Room.create(user_id: 1, description: "Living Room", width: 112, depth: 1253)
+Room.create(user_id: 2, description: "Kitchen", width: 42, depth: 5)
+Room.create(user_id: 2, description: "Dining Room", width: 51, depth: 23)
+Room.create(user_id: 2, description: "Rec Room", width: 25, depth: 52)
+Room.create(user_id: 3, description: "Library", width: 125, depth: 234)
+Room.create(user_id: 3, description: "Pantry", width: 125, depth: 542)
+Room.create(user_id: 3, description: "Family Room", width: 125, depth: 235)
