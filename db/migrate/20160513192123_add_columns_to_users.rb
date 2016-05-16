c0a73e1d14b5562769ef917986cccc985448a6e4
coltonstaab1 <colton.staab@gmail.com>
2016-05-15 21:40:49 -0400

    Modified the users table, the controller logic for the user still isn't working quite right though...

--- db/migrate/20160513192123_add_columns_to_users.rb
+++ db/migrate/20160513192123_add_columns_to_users.rb
@@ -2,5 +2,8 @@ class AddColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
+    add_column :users, :name, :string
+    add_column :users, :image, :string
+    add_column :users, :designer, :boolean, default: :false
   end
 end
