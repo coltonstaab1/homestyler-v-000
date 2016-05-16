10e2d2f6176203121418faa47fd72928b80b1af1
coltonstaab1 <colton.staab@gmail.com>
2016-05-16 18:43:28 -0400

    Created nested attribute new furniture form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,6 +11,26 @@ class RoomsController < ApplicationController
 
   def new
     @room = Room.new
+    @room.furniture_pieces.build(room_id: @room.id)
+  end
+
+  def create
+    binding.pry
+    @room = Room.create(room_params)
+    redirect_to room_path(@room)
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(
+      :user_id,
+      :description,
+      :width,
+      :depth, 
+      furniture_pieces_attributes: [
+        :room_id, 
+        :furniture_id
+      ])
   end
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ActiveRecord::Base
   belongs_to :user
   has_many :furniture_pieces
+  accepts_nested_attributes_for :furniture_pieces
+
 end
