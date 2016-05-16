5d8892176b8a09cb96fae399a5ff21de58b40622
coltonstaab1 <colton.staab@gmail.com>
2016-05-15 21:46:17 -0400

    Fixed the user routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  resource :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
+  resources :rooms
 end
