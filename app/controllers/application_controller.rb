da15499a0b643474fb4213fc4262c3384bbc5874
coltonstaab1 <colton.staab@gmail.com>
2016-05-17 17:00:38 -0400

    Added flash message successfully

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  around_filter :catch_not_found
+
+  private
+
+  def catch_not_found
+    yield
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_url, :flash => { :error => "Record not found." }
+  end
 end
