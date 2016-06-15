3a7271d70f3ced45039ced052dbc7fc429d242d7
coltonstaab1 <colton.staab@gmail.com>
2016-06-15 11:02:33 -0400

    Updated route nesting to leverage correct nested resource syntax

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :users, only: [:index, :show, :edit, :update] do 
     resources :rooms, only: [:index, :show, :edit, :new, :update]
+    resources :furnitures, only: [:new, :create]
   end
   resources :rooms
   resources :furnitures
   get '/missing_page', to: 'static#missing_page'
   get '*path' => redirect('/missing_page')
-  get '/users/:id/furnitures/new', to: 'furnitures#new'
-  post '/users/:id/furnitures/', to: 'furnitures#create'
 end
